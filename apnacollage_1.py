58fed237c1f818b6d7bddbb48b29d2508d0013cd
RajGandhi93339 <RajGandhi93339@gmail.com>
2024-11-21 17:21:15 +0530

    21-11-2024 Learn python

---
+++ apnacollage_1.py
@@ -0,0 +1,34 @@
+import time
+import selenium.webdriver
+
+
+option = 1
+name = "abc"
+city = "XYZ"
+age = 18
+Order = "XYZ"
+price = 00.00
+Passkey = True
+
+
+# print("Enter the name : ", input(name))
+# print("Enter the city :", input(city))
+# print("Enter the age :", input(age))
+# print("U want order option : /n YES/NO : ", input(Passkey))
+# print("Calculate price :", price)
+
+# Arithmetic operators
+a = 5
+b = 2
+
+Sum = a + b
+print(sum)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) #remainder
+print(a**b) #a^b = 5^2 = 25
+
+# relational operator
+
