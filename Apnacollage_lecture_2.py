7f653c67437cf82be6beecaebfdc6a7055e00e89
RajGandhi93339 <RajGandhi93339@gmail.com>
2024-11-22 15:47:02 +0530

    new

--- Apnacollage_lecture_2.py
+++ Apnacollage_lecture_2.py
@@ -1,9 +1,17 @@
-str1 = "This is a string.\nWe are creating a new program in Lecture2."
-print(str1)
+'''str1 = "This is a string.\nWe are creating a new program in Lecture2."
+print(str1)'''
 
 # Basic operators
-str2 = "Apna"
+'''str2 = "Apna"
 str3 = "collage"
 str4 = str2 + str3
 print(str2 + " " + str3)  # concatenation example
-print(len(str4))  # length of str
+print(len(str4))  # length of str'''
+
+# indexing 
+
+str5 = input("enter the word : ")
+ch =str5[0]
+print(ch)
+print(str5[0:4])
+
