856d2a854e18916d40db0da8f3b58fbb0a90f6ce
RajGandhi93339 <RajGandhi93339@gmail.com>
2024-11-23 09:41:00 +0530

    Save

---
+++ Apnacollage_Lecture_2Practice.py
@@ -0,0 +1,14 @@
+# WAP to input user's firstname & print its length.
+
+'''firstname = input("Enter first name :")
+lastname = input("Enter Last name :")
+
+print("Your firstname length is :",len(firstname))'''
+
+# WAP to find the occurrence of $ in a string.
+
+'''d = "$1 $2 $3 $4 $5"
+print(d)
+print(d.count("$"))'''
+
+
--- Apnacollage_lecture_2.py
+++ Apnacollage_lecture_2.py
@@ -10,9 +10,35 @@ print(len(str4))  # length of str"""
 
 # indexing
 
-str5 = input("enter the word : ")
+'''str5 = input("enter the word : ")
 ch = str5[0]
 print(ch)
 print(str5[0:4])
 print(str5[:4])  # bydefault getting [0:4]ge
-print(str5[0:])  # bydefault getting [0 : len(str)]
+print(str5[4:])  # bydefault getting [0 : len(str)]'''
+
+#  Negative indexing 
+'''Example
+ A  p  p  l  e
+-5 -4 -3 -2 -1
+'''
+
+# String functions
+'''str = "apple"
+print(str[-5:])
+
+value = "i am studying python from apna collage"
+print(value.endswith("age")) #ends name with in string age. #Output True 
+print(value.endswith("iam")) #value false
+
+print(str.capitalize()) #first letter is capitalize
+print(str) #sting value is not change it is as it is.
+
+print(value.replace("python","JavaScript")) #Replace string value Python to javascript
+'''
+str = "Hello"
+value = "i am from studying python from apna collage"
+
+print(str.find("l")) #find the word counting in current string
+print(value.count("from")) #find the word counting in current string
+
