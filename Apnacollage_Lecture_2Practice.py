59ce9152fef1a106f5f24bef54a26f7c047b2bec
RajGandhi93339 <RajGandhi93339@gmail.com>
2024-11-23 10:14:00 +0530

    Save

--- Apnacollage_Lecture_2Practice.py
+++ Apnacollage_Lecture_2Practice.py
@@ -1,14 +1,12 @@
 # WAP to input user's firstname & print its length.
 
-'''firstname = input("Enter first name :")
+"""firstname = input("Enter first name :")
 lastname = input("Enter Last name :")
 
-print("Your firstname length is :",len(firstname))'''
+print("Your firstname length is :",len(firstname))"""
 
 # WAP to find the occurrence of $ in a string.
 
-'''d = "$1 $2 $3 $4 $5"
+"""d = "$1 $2 $3 $4 $5"
 print(d)
-print(d.count("$"))'''
-
-
+print(d.count("$"))"""
