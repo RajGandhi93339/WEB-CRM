da5f47c0c10000790236e4a3b369d830825a47bc
RajGandhi93339 <RajGandhi93339@gmail.com>
2024-11-25 16:18:11 +0530

    Save

--- Python files/Apnacollage_lecture_2Conditional_Statement.py
+++ Python files/Apnacollage_lecture_2Conditional_Statement.py
@@ -1,7 +1,7 @@
-#Conditional statement
-#if_elif_else
+# Conditional statement
+# if_elif_else
 
-'''light = "none"
+"""light = "none"
 if light == "red":
     print("Stop")
 elif light == "Green":
@@ -11,10 +11,10 @@ elif light == "Yellow":
 else:
     (print("light is not working"))
 
-print("program is stop")'''
+print("program is stop")"""
 
 # Grade student based on marks
-stdname = input("ENTER STUDENT NAME :")
+"""stdname = input("ENTER STUDENT NAME :")
 marks = int(input("ENTER MARKS :"))
 
 # Std1 = "akash"
@@ -29,12 +29,23 @@ marks = int(input("ENTER MARKS :"))
 # Std5 = "Kevin"
 # marks4 = "55"
 
-if(marks >= 90):
+if marks >= 90:
     print("A Grade")
-elif(marks >= 80):
+elif marks >= 80 and marks < 90:
     print("B Grade")
-elif(marks >= 70):
+elif marks >= 70 and marks < 80:
     print("C Grade")
-elif(marks >= 60):
+elif marks >= 60 and marks < 70:
     print("D Grade")
-else:print("Not eligible for next grade....")
+else:
+    print("Not eligible for next Grade....")"""
+
+# nested if else
+'''age = 79
+if age >= 18:
+    if age >= 80:
+        print("can not drive")
+    else:
+        print("can drive")
+else:
+    print("not drive")'''
