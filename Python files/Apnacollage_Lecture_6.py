c02d2ba629e627b88670add46e4b9659c60a8152
RajGandhi93339 <RajGandhi93339@gmail.com>
2024-12-05 10:08:16 +0530

    save

--- Python files/Apnacollage_Lecture_6.py
+++ Python files/Apnacollage_Lecture_6.py
@@ -1,6 +1,5 @@
-
 # Functions in PYTHON
-
+"""
 def cal_Sum(i,j):
     sum = i + j
     print(sum)
@@ -15,3 +14,15 @@ cal_Sum(25,25)
 cal_Sum(50,50)
 
 cal_Sum(2,3)
+"""
+# Recursion in Python
+# when a functions call itself countinues or repeatedly.
+n = 5
+
+def show(n):
+    if n == 0:
+        return
+    print(n)
+    show(n - 1)  # Recursive function
+
+show(10)
